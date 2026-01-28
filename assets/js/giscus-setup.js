f6ff6e0c8f4bda9f46cb54fbc76ff28eeee386c2
juandaviddelosriosmahecha <151406776+juandaviddelosriosmahecha@users.noreply.github.com>
2026-01-28 02:12:48 +0000

    Deploying to gh-pages from @ juandaviddelosriosmahecha/juandaviddelosriosmahecha.github.io@bd36edc43f921b24e1b0f5a2603c76f6806c5f12 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "juandelosriosm/juandelosriosm.github.io",
+    "data-repo": "juandaviddelosriosmahecha/juandaviddelosriosmahecha.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
